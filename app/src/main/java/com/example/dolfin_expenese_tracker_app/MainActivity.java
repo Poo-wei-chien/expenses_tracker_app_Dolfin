ac4db1ec389283838473d116cd7a4db778460309
Poo Wei Chien <92501486+Poo-wei-chien@users.noreply.github.com>
2022-12-24 12:33:38 +0800

    New project to push to Github

---
+++ app/src/main/java/com/example/dolfin_expenese_tracker_app/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.dolfin_expenese_tracker_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
